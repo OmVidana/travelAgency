69a0a95553e8d9344c64511e2110e671c5d003cb
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-11-20 17:37:04 -0600

    NavBar without Map()

--- src/components/global/NavBar.jsx
+++ src/components/global/NavBar.jsx
@@ -1,4 +1,3 @@
-import { NavBarContent } from '../NavBarContent';
 import './NavBar.scss'
 
 function NavBar() {
@@ -8,13 +7,18 @@ function NavBar() {
                 <a className='navBar-space-main' href='/'>Yorick Travels <i className='navBar-main-icon fa-solid fa-plane-up'></i></a>
                 <div className='navBar-space-content'>
                     <ul className='navBar-space-elements'>
-                        {NavBarContent.map((item, index) => {
-                            return(
-                                    <li key={index}>
-                                        <a className={item.className} href={item.url}><i className={item.icon}></i>{item.title}</a>
-                                    </li>
-                            );
-                        })}
+                        <li>
+                            <a className="navBar-space-page" href="/"><i className="navBar-icons fa-sharp fa-solid fa-house"></i>Home</a>
+                        </li>
+                        <li>
+                            <a className="navBar-space-page" href="/search"><i className="navBar-icons fa-sharp fa-solid fa-search"></i>Vuelos</a>
+                        </li>
+                        <li>
+                            <a className="navBar-space-page" href="/hoteles"><i className="navBar-icons fa-solid fa-hotel"></i>Hoteles</a>
+                        </li>
+                        <li>
+                            <a className="navBar-space-page" href="/signup"><i className="navBar-icons fa-solid fa-user"></i>Sign Up</a>
+                        </li>
                     </ul>
                     <a className='login-button' href='/login'>Log In</a>
                 </div>

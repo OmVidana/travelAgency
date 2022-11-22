69a273f39d7d2699f1ba860340642d23cf162c3d
woryx7 <miguel.herrerap@liceodelvalle.edu.mx>
2022-11-21 20:28:08 -0600

    added my account

--- src/pages/AccountFlights.jsx
+++ src/pages/AccountFlights.jsx
@@ -1,13 +1,45 @@
 import NavBar from "../components/global/NavBar";
 import FlightCard from "../components/Results/FlightCard";
 import "./AccountFlights.scss";
+import {useContext, useEffect, useState} from "react";
+import AuthContext from "../context/AuthContext";
+import flightCard from "../components/Results/FlightCard";
+
 
 function AccountFlights() {
+    const [data, setData] = useState([])
+    let {user} = useContext(AuthContext)
+
+    useEffect(() =>{
+
+            // Send data to the backend via POST
+            fetch(`http://127.0.0.1:8000/vuelos/usuario/${user.username}`, {
+
+                method: 'GET',
+                mode: 'cors',
+
+            })
+                .then(response => response.json())
+                // .then(data => console.log(data['data']));
+                .then(data => {
+                    console.log(data)
+                    setData(data)
+                });
+    },[])
+
     return (
         <>    
             <NavBar/>
             <div className="accountflights-page">
-                <FlightCard/>
+            {data.map((flight, index) => (
+                <FlightCard
+                    key={index}
+                    arrival={flight['Salida']}
+                    departure={flight['Llegada']}
+                    arrivalt={flight['Tiempo_llegada'].slice(0,-1).split('T')[1].slice(0,-3)}
+                    departuret={flight['Tiempo_salida'].slice(0,-1).split('T')[1].slice(0,-3)}
+                />
+             ))}
             </div>
         </>
     );
